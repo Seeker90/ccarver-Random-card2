27303769cd754e2f6af9a0c4314d8504912614e0
Seeker90 <chris.carver90@icloud.com>
2025-08-29 23:48:17 +0000

    first

--- src/app.js
+++ src/app.js
@@ -6,18 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  
 
-function getRandomCard() {
-  let suits = ["Hearts","Spades","Clubs","Diamond"];
-  let values = ["2", "3", "4", "5", "6", "7", "8" "9", "10", "J", "Q" "K", "A"];
+  const suit = ["♠","♥","♣","♦"];
+  const value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
-  let suit = suits[Math.floor(Math.random() * suits.length)];
-  let value = values[Math.floor(Math.random() * values.length)]
+  let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+  let randomValue = value[Math.floor(Math.random() * value.length)];
   
-  return {suit, value };
-  
-  
+  if (randomSuit === "♥" || randomSuit ==="♦") {
+    document.querySelector(".top-suit").classList.add("text-danger");
+    document.querySelector(".bottom-suit").classList.add("text-danger");
+  }
+
+  document.querySelector(".top-suit").innerHTML = [randomSuit];
+  document.querySelector(".card-value").innerHTML = [randomValue];
+  document.querySelector(".bottom-suit").innerHTML = [randomSuit];
   
   }
 
@@ -44,5 +48,3 @@ function getRandomCard() {
 
 
 
-
-};
